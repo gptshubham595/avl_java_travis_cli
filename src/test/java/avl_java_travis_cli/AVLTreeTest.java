db04f77b4fee9de0f8fc81bae8bd0c24887959e9
Shubham gupta Ggps <24877361+gptshubham595@users.noreply.github.com>
2022-04-13 10:08:14 +0530

    Update AVLTreeTest.java

--- src/test/java/avl_java_travis_cli/AVLTreeTest.java
+++ src/test/java/avl_java_travis_cli/AVLTreeTest.java
@@ -1,11 +1,11 @@
-package test.java.avl_java_travis;
+package test.java.avl_java_travis_cli;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
-import main.java.avl_java_travis.AVLTree;
+import main.java.avl_java_travis_cli.AVLTree;
 
 public class AVLTreeTest {
 
